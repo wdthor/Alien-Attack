8c48c59d81b175dac8a3be094f3a562e712be38a
wdthor <wdthor@proton.me>
2023-05-19 19:48:10 +0200

    7- Creating references to nodes

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -4,6 +4,14 @@ var speed = 500
 
 var rocket_scene = preload("res://scenes/rocket.tscn")
 
+# @onready is a shortcut for _ready() function
+# $RocketContainer is a shortcut for get_node("RocketContainer")
+@onready var rocket_container = $RocketContainer
+
+#var rocket_container
+#func _ready():
+#	rocket_container = get_node("RocketContainer")
+
 func _process(delta):
 	if Input.is_action_just_pressed("shoot"):
 		shoot()
@@ -25,6 +33,11 @@ func _physics_process(delta):
 	global_position = global_position.clamp(Vector2(0, 0), screen_size)
 
 func shoot():
+#	Instantiate a rocket at the Player's position
 	var rocket_instance = rocket_scene.instantiate()
-	add_child(rocket_instance)
+#	add_child(rocket_instance)
+#	instantiate a rocket at the RocketContainer's position
+	rocket_container.add_child(rocket_instance)
+#	set the rocket position to the Player position
+	rocket_instance.global_position = global_position
 	rocket_instance.global_position.x += 80
