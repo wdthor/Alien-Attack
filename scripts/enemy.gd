411030dd5356c753937e3fd3312a23954988d6f2
wdthor <wdthor@proton.me>
2023-05-21 18:22:11 +0200

    15 - Lives & custom signals

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -7,3 +7,9 @@ func _physics_process(delta):
 
 func die():
 	queue_free()
+
+func _on_body_entered(body):
+#	The player takes damage
+	body.take_damage()
+#	The enemy dies
+	die()
--- scripts/game.gd
+++ scripts/game.gd
@@ -1,4 +1,13 @@
 extends Node2D
 
+var lives = 3
+
+@onready var player = $Player
+
 func _on_area_2d_area_entered(area):
 	area.die()
+
+func _on_player_took_damage():
+	lives -= 1
+	if lives == 0:
+		player.die()
